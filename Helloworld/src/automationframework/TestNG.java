a77e78a417e3e3c74a035a3789dad600a0ed93c9
Rishikasowmya <sowmyacherry12@gmail.com>
2018-09-05 15:37:17 +1000

    added the selenium project
    added the selenium project

---
+++ Helloworld/src/automationframework/TestNG.java
@@ -0,0 +1,29 @@
+package automationframework;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+public class TestNG {
+  @Test
+  public void userlogin() {
+	  
+	  System.out.println("login test is successful");
+  }
+  
+  
+}
+  
+
+  
+	  
+	  
+	  
+	  
+
+
+  
+  
+  
+
+
